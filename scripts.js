011a19bdc55e0540877877b9d5f0167479ab8f3c
FeliGeek <felipecorreaguzman@gmail.com>
2026-05-30 01:42:29 -0400

    dsadas

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
-const boton1 = document.getElementById("boton1");
-const boton2 = document.getElementById("boton2");
+const check1 = document.getElementById("check1");
+const eliminar1 = document.getElementById("eliminar1");
 
-const guardarBoton1 = localStorage.getItem("boton1");
+const guardarCheck1 = localStorage.getItem("check1");
 
-if(guardarBoton1){
-    boton1.textContent = guardarBoton1;
+if(guardarCheck1){
+    check1.textContent = guardarCheck1;
 }
 
-boton1.addEventListener("click", function(){
+check1.addEventListener("click", function(){
 
-    boton1.textContent = "1 ✔️";
+    check1.textContent = "1 ✔️";
 
-    localStorage.setItem("boton1", "1 ✔️");
+    localStorage.setItem("check1", "1 ✔️");
 
 });
 
-boton2.addEventListener("click", function(){
+eliminar1.addEventListener("click", function(){
 
-    boton1.textContent = "1";
+    check1.textContent = "1";
 
-    localStorage.removeItem("boton1");
+    localStorage.removeItem("check1");
 
 });
