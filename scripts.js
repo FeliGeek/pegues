0d0c9aa3e857294fea90043764544c84b65c5edf
FeliGeek <felipecorreaguzman@gmail.com>
2026-05-30 01:28:40 -0400

    kmaskdmkla

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
 const boton1 = document.getElementById("boton1");
 const boton2 = document.getElementById("boton2");
 
-boton1.addEventListener("click",function(){
+const guardarBoton1 = localStorage.getItem("boton1");
+
+if(guardarBoton1){
+    boton1.textContent = guardarBoton1;
+}
+
+boton1.addEventListener("click", function(){
+
     boton1.textContent = "1 ✔️";
+
+    localStorage.setItem("boton1", "1 ✔️");
+
 });
 
-boton2.addEventListener("click",function(){
+boton2.addEventListener("click", function(){
+
     boton1.textContent = "1";
-});
 
+    localStorage.removeItem("boton1");
+
+});
